63d404e31918d844ba68bf6febd165392d15c1ee
DiegoMaldonado0 <diego.maldonado5160@alumnos.udg.mx>
2025-05-03 00:29:19 -0600

    fix: añadir basename en el router de React para resolver problemas de rutas en Netlify

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // App.jsx
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
@@ -12,19 +12,21 @@ import UserProfile from "./components/UserProfile";
 
 const App = () => {
   return (
-    <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-      <Header />
-      <Routes>
-        <Route path="/Hands-AI" element={<Hero />} />
-        <Route path="/Hands-AI/login" element={<Login />} />
-        <Route path="/Hands-AI/signup" element={<SignUp />} />
-        <Route path="/Hands-AI/simon-dice" element={<Test />} />
-        <Route path="/Hands-AI/word-builder" element={<WordBuilder />} />
-        <Route path="/Hands-AI/badges" element={<Badges />} />
-        <Route path="/Hands-AI/profile" element={<UserProfile />} />
-      </Routes>
-      <Footer />
-    </div>
+    <Router basename="/Hands-AI">
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/simon-dice" element={<Test />} />
+          <Route path="/word-builder" element={<WordBuilder />} />
+          <Route path="/badges" element={<Badges />} />
+          <Route path="/profile" element={<UserProfile />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 };
 
